c954092831f3309f2d3e60dba11cb2d61ab14236
Ayush <ayushrajput000031@gmail.com>
2025-03-26 10:25:07 +0530

    added error handling

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -26,7 +26,7 @@ exports.addIncome = async(req,res)=>{
 
         res.status(200).json({newIncome})
     } catch (err) {
-        res.status(500).json({message : "Server Error"})
+        res.status(500).json({message : "Server Error" , error : err.message})
     }
 }
 
